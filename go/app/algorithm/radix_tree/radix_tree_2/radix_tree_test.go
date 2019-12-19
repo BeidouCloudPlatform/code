ea364e3be93604404d2b3ba192ff035a6567462c
Xiang Liu <lx1036@126.com>
2019-12-19 14:37:03 +0800

    xxx

--- go/app/algorithm/radix_tree/radix_tree_2/radix_tree_test.go
+++ go/app/algorithm/radix_tree/radix_tree_2/radix_tree_test.go
@@ -11,22 +11,24 @@ https://github.com/armon/go-radix/blob/master/radix_test.go
 */
 func TestRadix2(test *testing.T) {
 	dictionary := make(map[string]interface{})
-	for i := 0; i < 1000; i++ {
+	/*for i := 0; i < 1000; i++ {
 		id := generateUUID()
 		dictionary[id] = i
-	}
+	}*/
+	dictionary["h"] = 1
+	dictionary["he"] = 2
 
-	tree := NewFromMap(dictionary)
+	tree := NewFromMap2(dictionary)
 	if tree.Len() != len(dictionary) {
 		test.Fatalf("wrong length: want %d got %d", len(dictionary), tree.Len())
 	}
 
-	tree.Walk(func(key string, value interface{}) bool {
+	/*tree.Walk(func(key string, value interface{}) bool {
 		println(key)
 		return false
-	})
+	})*/
 
-	for key, value := range dictionary {
+	/*for key, value := range dictionary {
 		out, ok := tree.Get(key)
 		if !ok {
 			test.Fatalf("missing key: %v", key)
@@ -35,7 +37,7 @@ func TestRadix2(test *testing.T) {
 		if out != value {
 			test.Fatalf("value mismatch, want %v got %v", value, out)
 		}
-	}
+	}*/
 
 	/*outMin, _, _ := tree.Min()
 	if outMin != min {
@@ -45,6 +47,52 @@ func TestRadix2(test *testing.T) {
 	if outMax != max {
 		test.Fatalf("bad max want %v got %v", max, outMax)
 	}*/
+}
+func NewFromMap2(dictionary map[string]interface{}) *Tree {
+	tree := &Tree{
+		root: &Node{},
+	}
+	for key, value := range dictionary {
+		tree.Insert2(key, value)
+	}
+	
+	return tree
+}
+func (tree *Tree) Insert2(key string, value interface{}) (interface{}, bool) {
+	var parent *Node
+	search := key
+	rootNode := tree.root
+	for  {
+		if len(search) == 0 {
+		
+		}
+		
+		parent = rootNode
+		rootNode = rootNode.getEdge(search[0])
+		if rootNode == nil { // rootNode has no edge, create one edge
+			parent.addEdge(Edge{
+				label: search[0],
+				node:  &Node{
+					leaf:   &LeafNode{
+						key:   search,
+						value: value,
+					},
+					prefix: search,
+					edges:  nil,
+				},
+			})
+			tree.size++
+			return nil, false
+		}
+		
+		commonPrefix := longestPrefix(search, rootNode.prefix)
+		if commonPrefix == len(rootNode.prefix) {
+			search = search[commonPrefix:]
+			continue
+		}
+	}
+	
+	return nil, false
 }
 
 func TestRoot(test *testing.T) {
--- go/app/algorithm/radix_tree/radix_tree_2/tree.go
+++ go/app/algorithm/radix_tree/radix_tree_2/tree.go
@@ -1,28 +1,104 @@
 package radix_tree_2
 
-import "strings"
+import (
+	"sort"
+	"strings"
+)
+
+type Edges []Edge
+
+// Edge node
+type Edge struct {
+	label byte
+	node  *Node
+}
+
+func (edges Edges) Len() int {
+	return len(edges)
+}
+
+func (edges Edges) Less(i, j int) bool {
+	return edges[i].label < edges[j].label
+}
+
+func (edges Edges) Swap(i, j int) {
+	edges[i], edges[j] = edges[j], edges[i]
+}
+
+func (edges Edges) Sort() {
+	sort.Sort(edges)
+}
+
+type Node struct {
+	// store leaf
+	leaf   *LeafNode
+	prefix string
+	edges  Edges
+}
+
+type LeafNode struct {
+	key   string
+	value interface{}
+}
+
+func (node *Node) isLeaf() bool {
+	return node.leaf != nil
+}
+
+func (node *Node) getEdge(label byte) *Node {
+	number := len(node.edges)
+	idx := sort.Search(number, func(i int) bool {
+		return node.edges[i].label >= label
+	})
+	
+	if idx < number && node.edges[idx].label == label {
+		return node.edges[idx].node
+	}
+	
+	return nil
+}
+
+func (node *Node) addEdge(edge Edge) {
+	node.edges = append(node.edges, edge)
+	node.edges.Sort()
+}
+
+func (node *Node) updateEdge(label byte, newNode *Node) {
+	number := len(node.edges)
+	idx := sort.Search(number, func(i int) bool {
+		return node.edges[i].label >= label
+	})
+	
+	if idx < number && node.edges[idx].label == label {
+		node.edges[idx].node = newNode
+		return
+	}
+	
+	panic("missing edge")
+}
+
 
 type Tree struct {
-	root *node
+	root *Node
 	size int
 }
 
 type WalkFn func(key string, value interface{}) bool
 
 func (tree *Tree) Insert(key string, value interface{}) (interface{}, bool) {
-	var parent *node
+	var parent *Node
 	search := key
 	rootNode := tree.root
 
 	for {
-		if len(search) == 0 {
+		if len(search) == 0 { // substring key util key is ""
 			if rootNode.isLeaf() {
 				old := rootNode.leaf.value
 				rootNode.leaf.value = value
 				return old, true
 			}
 
-			rootNode.leaf = &leafNode{
+			rootNode.leaf = &LeafNode{
 				key:   search,
 				value: value,
 			}
@@ -33,10 +109,10 @@ func (tree *Tree) Insert(key string, value interface{}) (interface{}, bool) {
 		parent = rootNode
 		rootNode = rootNode.getEdge(search[0])
 		if rootNode == nil { // No edge, create one
-			edge := edge{
+			edge := Edge{
 				label: search[0],
-				node: &node{
-					leaf: &leafNode{
+				node: &Node{
+					leaf: &LeafNode{
 						key:   search,
 						value: value,
 					},
@@ -56,20 +132,20 @@ func (tree *Tree) Insert(key string, value interface{}) (interface{}, bool) {
 
 		// Split the node
 		tree.size++
-		child := &node{
+		child := &Node{
 			prefix: search[:commonPrefix],
 		}
 		parent.updateEdge(search[0], child)
 
 		// Restore the existing node
-		child.addEdge(edge{
+		child.addEdge(Edge{
 			label: rootNode.prefix[commonPrefix],
 			node:  rootNode,
 		})
 		rootNode.prefix = rootNode.prefix[commonPrefix:]
 
 		// if the new key is a subnet, add it into this node
-		leaf := &leafNode{
+		leaf := &LeafNode{
 			key:   search,
 			value: value,
 		}
@@ -78,9 +154,9 @@ func (tree *Tree) Insert(key string, value interface{}) (interface{}, bool) {
 			child.leaf = leaf
 			return nil, false
 		}
-		child.addEdge(edge{
+		child.addEdge(Edge{
 			label: search[0],
-			node: &node{
+			node: &Node{
 				leaf:   leaf,
 				prefix: search,
 			},
@@ -116,7 +192,7 @@ func (tree *Tree) Get(key string) (interface{}, bool) {
 func (tree *Tree) Delete(key string) (interface{}, bool) {
 	search := key
 	rootNode := tree.root
-	var parent *node
+	var parent *Node
 	for {
 		if len(search) == 0 {
 			if !rootNode.isLeaf() {
@@ -165,4 +241,58 @@ func (tree *Tree) Len() int {
 
 func (tree *Tree) Walk(fn WalkFn) {
 	recursiveWalk(tree.root, fn)
+}
+
+
+func longestPrefix(k1 string, k2 string) int {
+	max := len(k1)
+	if l := len(k2); l < max {
+		max = l
+	}
+	
+	var i int
+	for i = 0; i < max; i++ {
+		if k1[i] != k2[i] {
+			break
+		}
+	}
+	
+	return i
+}
+
+/*func (tree *Tree) Min() (string, interface{}, bool)  {
+
+}
+
+func (tree *Tree) Max() (string, interface{}, bool)  {
+
+}*/
+
+func New() *Tree {
+	return NewFromMap(nil)
+}
+
+func NewFromMap(dictionary map[string]interface{}) *Tree {
+	tree := &Tree{
+		root: &Node{},
+	}
+	for key, value := range dictionary {
+		tree.Insert(key, value)
+	}
+	
+	return tree
+}
+
+func recursiveWalk(node *Node, fn WalkFn) bool {
+	if node.leaf != nil && fn(node.leaf.key, node.leaf.value) {
+		return true
+	}
+	
+	for _, edge := range node.edges {
+		if recursiveWalk(edge.node, fn) {
+			return true
+		}
+	}
+	
+	return false
 }
